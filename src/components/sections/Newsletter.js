394f8de4ede8b2905cb26744ebcb2ec568f39488
edmundweir <edmund.weir@gmail.com>
2021-04-19 16:44:53 +1000

    first commit

---
+++ src/components/sections/Newsletter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Newsletter() {
+    return (
+        <div id="reader's club" class="container" style={{ margin: '24px'}}>
+            <div class="ml-form-embed"
+            data-account="2495824:w5x8z5w2r4"
+            data-form="3947551:v5n4b0">
+            </div>
+            <br />
+            <br />
+            <center><p onClick={scrollToTop} style={{ cursor: 'pointer', }}>
+                Return to top.
+            </p></center>
+        </div>
+    )
+}
+
+const scrollToTop = () => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth' })
+  }
+
+export default Newsletter;
