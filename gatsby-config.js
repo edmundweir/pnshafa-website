2de50bf84373aba21363b288294f200534891518
edmundweir <edmund.weir@gmail.com>
2021-04-27 10:01:39 +1000

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  pathPrefix: "/pnshafa-website",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -63,4 +62,5 @@ module.exports = {
       },
     },
   ],
+  pathPrefix: "/pnshafa-website",
 };
